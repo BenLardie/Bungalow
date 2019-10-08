7ca92bbf6d9684a0760474176516bce468a6d683
Ben Lardie <benlardie@gmail.com>
2019-10-08 10:23:00 -0400

    fixed propmt

--- bungalow_api/views.py
+++ bungalow_api/views.py
@@ -10,7 +10,7 @@ def house_upload(request):
     template = 'house_upload.html'
 
     promt = {
-        'order': 'Order of the CSV should be'
+        'order': 'Order of the CSV should be area_unit, bathrooms, bedrooms, home_size, home_type, last_sold_date, last_sold_price, link, price, property_size, rent_price, rentzestimate_amount, rentzestimate_last_updated, tax_value, tax_year, year_built, zestimate_amount, zestimate_last_updated, zillow_id, address, city, state, zipcode'
     }
 
     if request.method == 'GET':
